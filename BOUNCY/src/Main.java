4a1ecc9f58c029aa6237a5c9805e09b96166123b
Renz Erik Lucena <renz_lucena@dlsu.edu.ph>
2024-06-21 02:57:05 +0800

    Update main - batch and fps

--- BOUNCY/src/Main.java
+++ BOUNCY/src/Main.java
@@ -41,53 +41,109 @@ public class Main extends Application {
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.BASELINE_CENTER);
 
-        Label labelX = new Label("Pos X:");
-        TextField inputX = new TextField();
-        Label labelY = new Label("Pos Y:");
-        TextField inputY = new TextField();
-        Label labelDeg = new Label("Degrees:");
-        TextField inputDeg = new TextField();
-        Label labelVel = new Label("Velocity (px/s):");
-        TextField inputVel = new TextField();
+        Label labelStartX = new Label("Start X:");
+        TextField inputStartX = new TextField();
+        Label labelStartY = new Label("Start Y:");
+        TextField inputStartY = new TextField();
+        Label labelEndX = new Label("End X:");
+        TextField inputEndX = new TextField();
+        Label labelEndY = new Label("End Y:");
+        TextField inputEndY = new TextField();
+        Label labelStartAngle = new Label("Start Angle:");
+        TextField inputStartAngle = new TextField();
+        Label labelEndAngle = new Label("End Angle:");
+        TextField inputEndAngle = new TextField();
+        Label labelStartVelocity = new Label("Start Velocity:");
+        TextField inputStartVelocity = new TextField();
+        Label labelEndVelocity = new Label("End Velocity:");
+        TextField inputEndVelocity = new TextField();
+        Label labelVelocity = new Label("Velocity (px/s):");
+        TextField inputVelocity = new TextField();
+        Label labelAngle = new Label("Angle (degrees):");
+        TextField inputAngle = new TextField();
+        Label labelCount = new Label("Number of Particles:");
+        TextField inputCount = new TextField();
 
-        Button btnSubmit = new Button("ADD BALL");
+        Button btnAddByDistance = new Button("Add by Distance");
+        Button btnAddByAngle = new Button("Add by Angle");
+        Button btnAddByVelocity = new Button("Add by Velocity");
 
         gridPane.setAlignment(Pos.BASELINE_CENTER);
-        gridPane.addRow(0, labelX, inputX);
-        gridPane.addRow(1, labelY, inputY);
-        gridPane.addRow(2, labelDeg, inputDeg);
-        gridPane.addRow(3, labelVel, inputVel);
+        gridPane.addRow(0, labelStartX, inputStartX, labelEndX, inputEndX);
+        gridPane.addRow(1, labelStartY, inputStartY, labelEndY, inputEndY);
+        gridPane.addRow(2, labelStartAngle, inputStartAngle, labelEndAngle, inputEndAngle);
+        gridPane.addRow(3, labelStartVelocity, inputStartVelocity, labelEndVelocity, inputEndVelocity);
+        gridPane.addRow(4, labelVelocity, inputVelocity);
+        gridPane.addRow(5, labelAngle, inputAngle);
+        gridPane.addRow(6, labelCount, inputCount);
 
         TextArea tester = new TextArea("(Test) Balls rn:\n");
         tester.setMaxSize(250, 720);
 
         GridPane gpControl = new GridPane();
         gpControl.addRow(0, gridPane);
-        gpControl.addRow(1, btnSubmit);
-        gpControl.addRow(2, tester);
+        gpControl.addRow(1, btnAddByDistance);
+        gpControl.addRow(2, btnAddByAngle);
+        gpControl.addRow(3, btnAddByVelocity);
+        gpControl.addRow(4, tester);
 
         paneControl.getChildren().add(gpControl);
         paneContainer.getChildren().addAll(paneControl, canvas, fpsLabel);
         Scene scene = new Scene(paneContainer, 1530, 720);
 
-        primaryStage.setTitle("BOUNCING BALLS - grp i forgot num");
+        primaryStage.setTitle("Particle Physics Simulator");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        btnSubmit.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                try {
-                    double x = Double.parseDouble(inputX.getText());
-                    double y = Double.parseDouble(inputY.getText());
-                    double velocity = Double.parseDouble(inputVel.getText());
-                    double degrees = Double.parseDouble(inputDeg.getText());
-                    addParticle(x, y, velocity, degrees);
-                    tester.appendText(n_balls + " at pos (" + x + "," + y + ") " + degrees + "deg + " + velocity + "px/s\n");
-                    n_balls++;
-                } catch (NumberFormatException e) {
-                    tester.appendText("Invalid input\n");
-                }
+        btnAddByDistance.setOnAction(event -> {
+            try {
+                double startX = Double.parseDouble(inputStartX.getText());
+                double startY = Double.parseDouble(inputStartY.getText());
+                double endX = Double.parseDouble(inputEndX.getText());
+                double endY = Double.parseDouble(inputEndY.getText());
+                double velocity = Double.parseDouble(inputVelocity.getText());
+                double angle = Double.parseDouble(inputAngle.getText());
+
+                int n = Integer.parseInt(inputCount.getText());
+                addParticlesByDistance(n, startX, startY, endX, endY, velocity, angle);
+                n_balls += n;
+                tester.appendText(n + " particles added with constant velocity and angle\n");
+            } catch (NumberFormatException e) {
+                tester.appendText("Invalid input\n");
+            }
+        });
+
+        btnAddByAngle.setOnAction(event -> {
+            try {
+                double startX = Double.parseDouble(inputStartX.getText());
+                double startY = Double.parseDouble(inputStartY.getText());
+                double startAngle = Double.parseDouble(inputStartAngle.getText());
+                double endAngle = Double.parseDouble(inputEndAngle.getText());
+                double velocity = Double.parseDouble(inputVelocity.getText());
+
+                int n = Integer.parseInt(inputCount.getText());
+                addParticlesByAngle(n, startX, startY, startAngle, endAngle, velocity);
+                n_balls += n;
+                tester.appendText(n + " particles added with constant start point and velocity\n");
+            } catch (NumberFormatException e) {
+                tester.appendText("Invalid input\n");
+            }
+        });
+
+        btnAddByVelocity.setOnAction(event -> {
+            try {
+                double startX = Double.parseDouble(inputStartX.getText());
+                double startY = Double.parseDouble(inputStartY.getText());
+                double startVelocity = Double.parseDouble(inputStartVelocity.getText());
+                double endVelocity = Double.parseDouble(inputEndVelocity.getText());
+                double angle = Double.parseDouble(inputAngle.getText());
+
+                int n = Integer.parseInt(inputCount.getText());
+                addParticlesByVelocity(n, startX, startY, startVelocity, endVelocity, angle);
+                n_balls += n;
+                tester.appendText(n + " particles added with constant start point and angle\n");
+            } catch (NumberFormatException e) {
+                tester.appendText("Invalid input\n");
             }
         });
 
@@ -103,8 +159,38 @@ public class Main extends Application {
         }.start();
     }
 
-    private void addParticle(double x, double y, double velocity, double angle) {
-        particles.add(new Particle(x, y, velocity, Math.toRadians(angle)));
+    private void addParticlesByDistance(int n, double startX, double startY, double endX, double endY, double velocity, double angle) {
+        double totalDistance = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
+        double dx = (endX - startX) / (n - 1);
+        double dy = (endY - startY) / (n - 1);
+
+        for (int i = 0; i < n; i++) {
+            double x = startX + i * dx;
+            double y = startY + i * dy;
+            particles.add(new Particle(x, y, velocity, Math.toRadians(angle)));
+        }
+    }
+
+    private void addParticlesByAngle(int n, double startX, double startY, double startAngle, double endAngle, double velocity) {
+        double angleDiff = (endAngle - startAngle) / (n - 1);
+
+        for (int i = 0; i < n; i++) {
+            double angle = startAngle + i * angleDiff;
+            double x = startX + velocity * Math.cos(Math.toRadians(angle));
+            double y = startY - velocity * Math.sin(Math.toRadians(angle)); // negative sin because Y increases downwards
+            particles.add(new Particle(x, y, velocity, Math.toRadians(angle)));
+        }
+    }
+
+    private void addParticlesByVelocity(int n, double startX, double startY, double startVelocity, double endVelocity, double angle) {
+        double velocityDiff = (endVelocity - startVelocity) / (n - 1);
+
+        for (int i = 0; i < n; i++) {
+            double velocity = startVelocity + i * velocityDiff;
+            double x = startX + velocity * Math.cos(Math.toRadians(angle));
+            double y = startY - velocity * Math.sin(Math.toRadians(angle)); // negative sin because Y increases downwards
+            particles.add(new Particle(x, y, velocity, Math.toRadians(angle)));
+        }
     }
 
     private void update(long now) {

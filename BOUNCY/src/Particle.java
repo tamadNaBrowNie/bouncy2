4ad8295261d26472c6e30d772d4c4c89620cc61b
Shawn <shawn_aberin@dlsu.edu.ph>
2024-06-22 02:22:26 +0800

    no more TL

--- BOUNCY/src/Particle.java
+++ BOUNCY/src/Particle.java
@@ -37,7 +37,7 @@ public class Particle  implements Callable<double[]> {
 		this.circle.setLayoutY ( this.y);
 		EventHandler<ActionEvent> foo = new Ctrlr(ppu*Math.cos(theta),-ppu* Math.sin(theta),this.circle);
 
-		this.tl = new Timeline(new KeyFrame(Duration.millis(17)),foo);
+		//this.tl = new Timeline(new KeyFrame(Duration.millis(17)),foo);
 
 	}
 
@@ -47,20 +47,20 @@ public class Particle  implements Callable<double[]> {
 	
 	@Override
 	public double[] call() throws Exception {
-//		if (this.x<=0||1280<=this.x){
-//			this.dx *=-1;
-//		}
-//		if (this.y<=0||this.y>=720){
-//			this.dy *=-1;
-//		}
-//		this.x += dx;
-//		this.y+= dy;this.circle.setCenterX ( this.x);
-//		this.circle.setCenterY ( this.y);
-//		this.circle.setLayoutX(this.x);
-//		this.circle.setLayoutY ( this.y);
-//		
-		 tl.setCycleCount(Timeline.INDEFINITE);
-	        tl.play();
+		if (this.x<=0||1280<=this.x){
+			this.dx *=-1;
+		}
+		if (this.y<=0||this.y>=720){
+			this.dy *=-1;
+		}
+		this.x += dx;
+		this.y+= dy;this.circle.setCenterX ( this.x);
+		this.circle.setCenterY ( this.y);
+		this.circle.setLayoutX(this.x);
+		this.circle.setLayoutY ( this.y);
+		
+		//  tl.setCycleCount(Timeline.INDEFINITE);
+	    //     tl.play();
 		double [] res = {this.x,this.y};
 		return res;
 	}

f9a635fa18f3bdd84082880c315c86210700895a
ContractorOz <xiaelis@gmail.com>
2024-06-22 00:22:26 +0800

    fixed compatibility

--- BOUNCY/src/Particle.java
+++ BOUNCY/src/Particle.java
@@ -18,7 +18,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-public class Particle extends Region implements Callable<double[]> {
+public class Particle implements Callable<double[]> {
 	
 	Graphics g;
 	Circle circle;
@@ -39,19 +39,20 @@ public class Particle extends Region implements Callable<double[]> {
 				new EventHandler<ActionEvent>() {
 			double dx = ppu*Math.cos(theta); //Step on x or velocity
         	double dy = -ppu* Math.sin(theta);
-	@Override
-            public void handle(ActionEvent t) {		
-if (this.x<=0||1280<=this.x){
-	this.dx *=-1;
-}
-if (this.y<=0||this.y>=720){
-	this.dy *=-1;
-}
-this.x += dx;
-this.y+= dy;
-this.circle.setLayoutX(this.x);
-this.circle.setLayoutY ( this.y);
-		});}
+        	
+        @Override
+        public void handle(ActionEvent t) {		
+			if (x<=0||1280<=x){
+				this.dx *=-1;
+			}
+			if (y<=0||y>=720){
+				this.dy *=-1;
+			}
+			this.x += dx;
+			this.y+= dy;
+			circle.setLayoutX(x);
+			circle.setLayoutY (y);
+					});}
 	}
 
 	public Circle getBall(){

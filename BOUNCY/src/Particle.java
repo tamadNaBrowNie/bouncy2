6873dce83d61f004bd09470cd3786ce0fa4d1833
Shawn <shawn_aberin@dlsu.edu.ph>
2024-06-21 16:09:04 +0800

    Particle now creates circle. to  make it move, call its call() method

--- BOUNCY/src/Particle.java
+++ BOUNCY/src/Particle.java
@@ -14,16 +14,20 @@ public class Particle implements Callable {
 	
 	Thread th;
 	Graphics g;
-	
+	Circle circle;
 	private double x,y;
 	private double dx,dy;
 	Particle(float x,float  y,
 			float theta, float v){ 
+		float ppu = v/60;
 		double rad = Math.toRadians(theta);
-		this.dx = v*Math.cos(rad);
-		this.dy = v* Math.sin(rad);
+		this.dx = ppu*Math.cos(rad);
+		this.dy = -ppu* Math.sin(rad);
 				this.x = x;
 				this.y = y;
+				this.circle = new Circle(10.f,Color.RED);
+				this.circle.setCenterX ( this.x);
+				this.circle.setCenterY ( this.y);
 	}
 //	
 //	public void draw(Graphics g)
@@ -34,15 +38,21 @@ public class Particle implements Callable {
 //	}
 //}
 
+private float collide(int max, double delta, double pos){
+	double future = delta+pos;
+	if (future<0||future>max)
+	return -1;
+	return 1;
+}
 	@Override
 	public Object call() throws Exception {
+		this.circle.setTranslateX(dx);
+		this.circle.setTranslateY(dy);
 		this.x += this.dx;
 		this.y+= this.dy;
-		if
-		 (this.x + this.dx> 1280||this.x + this.dx<0)
-		this.dx = -this.dx;
-		if (this.y + this.dy> 1280||this.y + this.dy<0)
-		this.dy = -this.dy;
+		
+		this.dx *= this.collide(1280,dx,x);
+		this.dy *= this.collide(720,dy,y);
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Unimplemented method 'call'");
 	}

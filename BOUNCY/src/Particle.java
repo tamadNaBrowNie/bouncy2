a861ba90dae51ebd2c83f8d1fd5be77ca3049dcf
Shawn <shawn_aberin@dlsu.edu.ph>
2024-06-21 23:49:31 +0800

    fixed the timeline

--- BOUNCY/src/Particle.java
+++ BOUNCY/src/Particle.java
@@ -1,5 +1,8 @@
 import java.awt.Graphics;
 import java.util.concurrent.Callable;
+
+import javax.swing.plaf.synth.Region;
+
 import javafx.scene.shape.Circle;
 import javafx.scene.transform.Translate;
 import javafx.scene.paint.Color;
@@ -37,7 +40,8 @@ public class Particle extends Region implements Callable<double[]> {
 				new EventHandler<ActionEvent>() {
 			double dx = ppu*Math.cos(rad); //Step on x or velocity
         	double dy = -ppu* Math.sin(rad);
-			
+	@Override
+            public void handle(ActionEvent t) {		
 if (this.x<=0||1280<=this.x){
 	this.dx *=-1;
 }
@@ -48,7 +52,7 @@ this.x += dx;
 this.y+= dy;
 this.circle.setLayoutX(this.x);
 this.circle.setLayoutY ( this.y);
-		});
+		});}
 	}
 
 	public Circle getBall(){

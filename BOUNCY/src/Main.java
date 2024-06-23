eeb354fb1d3244d2024995d790c5ff5a960a8a8b
Shawn <shawn_aberin@dlsu.edu.ph>
2024-06-23 15:54:09 +0800

    Allex this one is faster

--- BOUNCY/src/Main.java
+++ BOUNCY/src/Main.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 import javafx.scene.shape.Circle;
